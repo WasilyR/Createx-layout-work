27606ff76032b969c1edbc681e7758899afc8edc
WasilyR <127860653+WasilyR@users.noreply.github.com>
2023-04-17 15:49:15 +0300

    Add files via upload

---
+++ docs/js/main.js
@@ -0,0 +1,42 @@
+$(function () {
+
+	var mixer = mixitup('.directions__list');
+
+	$('.directions__filter-btn').on('click', function () {
+		$('.directions__filter-btn').removeClass('directions__filter-btn--active')
+		$(this).addClass('directions__filter-btn--active')
+	})
+
+	$('.team__slider').slick({
+		arrows: false,
+		slidesToShow: 4,
+		Infinity: true,
+		draggable: false,
+		waitForAnimate: false,
+	})
+
+	$('.team__slider-prev').on('click', function (e) {
+		e.preventDefault()
+		$('.team__slider').slick('slickPrev')
+	})
+	$('.team__slider-next').on('click', function (e) {
+		e.preventDefault()
+		$('.team__slider').slick('slickNext')
+	})
+
+	$('.testimonials__slider').slick({
+		arrows: false,
+		// dots: true,
+	})
+
+	$('.testimonials-prev').on('click', function (e) {
+		e.preventDefault()
+		$('.testimonials__slider').slick('slickPrev')
+	})
+
+	$('.testimonials-next').on('click', function (e) {
+		e.preventDefault()
+		$('.testimonials__slider').slick('slickNext')
+	})
+
+})
